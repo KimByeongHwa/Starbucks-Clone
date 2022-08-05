9bf1bde761fbde46c458c9b343ff097cba4b83ef
KimByeongHwa <iambyeonghwa@gmail.com>
2022-08-05 18:30:19 +0900

    0805 완성

--- js/main.js
+++ js/main.js
@@ -16,23 +16,39 @@ searchInputEl.addEventListener('blur', function(){
 });
 
 const badgeEl = document.querySelector('header .badges');
+const toTopEl = document.querySelector('#to-top');
 
 window.addEventListener('scroll', _.throttle(function(){
     console.log(window.scrollY);
     if(window.scrollY > 500){
+        //뱃지 숨기기
         //gsap.to(요소, 지속시간, 옵션);
         gsap.to(badgeEl, .6, {
             opacity: 0,
             display: 'none'
         })
+        //버튼 보이기
+        gsap.to(toTopEl, .2, {
+            x: 0
+        });
     } else{
         gsap.to(badgeEl, .6, {
             opacity: 1,
             display: 'block'
-        })
+        });
+        //버튼 숨기기
+        gsap.to(toTopEl, .2, {
+            x: 100
+        });
     }
 }, 300));
 // ._throttle(함수, 시간)
+
+toTopEl.addEventListener('click', function(){
+    gsap.to(window, .7, {
+        scrollTo: 0
+    });
+});
 
 
 const fadeEls = document.querySelectorAll('.visual .fade-in');
@@ -43,6 +59,8 @@ fadeEls.forEach(function(fadeEl, index){
         opacity: 1
     });
 });
+
+
 
 //new Swiper(선택자, 옵션);
 new Swiper('.notice-line .swiper-container',{
@@ -67,7 +85,17 @@ new Swiper('.promotion .swiper-container',{
         prevEl: '.promotion .swiper-prev',
         nextEl: '.promotion .swiper-next'
     }
-})
+});
+new Swiper('.awards .swiper-container',{
+    autoplay: true,
+    loop: true,
+    spaceBetween: 30,
+    slidesPerView: 5,
+    navigation: {
+        prevEl: '.awards .swiper-prev',
+        nextEl: '.awards .swiper-next'
+    }
+});
 
 const promotionEl = document.querySelector('.promotion');
 const promotionToggleBtn = document.querySelector('.toggle-promotion');
@@ -103,3 +131,19 @@ function floatingObject(selector, delay, size){
 floatingObject('.floating1', 1, 15);
 floatingObject('.floating2', .5, 15);
 floatingObject('.floating3', 1.5, 20);
+
+
+const spyEls = document.querySelectorAll('section.scroll-spy');
+spyEls.forEach(function(spyEl){
+    new ScrollMagic
+    .Scene({
+        triggerElement : spyEl, // 보여짐 여부를 감시할 요소를 지정
+        triggerHook: .8
+    })
+    .setClassToggle(spyEl, 'show')
+    .addTo(new ScrollMagic.Controller());
+});
+
+
+const thisYear = document.querySelector('.this-year');
+thisYear.textContent = new Date().getFullYear();
